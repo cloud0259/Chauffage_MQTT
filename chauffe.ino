b2ede3dfd6f4966837283bd01ac7456cea0501cd
cloud0259 <pilloystephane@gmail.com>
2018-11-19 21:59:16 +0100

    Ajout commentaire Chauffe.ino

--- chauffe.ino
+++ chauffe.ino
@@ -1,22 +1,23 @@
+/*************************************************/
+/*************CHAUFFE*****************************/
+/*************************************************/
 void chauffe()
 {
-  Serial.print("temp ");
-  Serial.println(t);
-  Serial.print("Cons :");
-  Serial.println(c);
-  if(t > 0.0)
+  Serial.print("temp "); //Ecrit temp dans le terminal
+  Serial.println(t); //Ecrit la temperature dans le terminal
+  Serial.print("Cons :"); //Ecrit cons dans le terminal
+  Serial.println(c); //Ecrit la consigne dans le terminal
+  if(t > 0.0) //Si la température est supérieur a 0 (évite les erreurs.
   {
-    if(t <= c -hysteresis)
+    if(t <= c -hysteresis) //Si la temperature est inférieur a la consigne - l'hysteresis
     {
-     Serial.println(c-hysteresis);
-      client.publish("DomoHouse","1");
+      client.publish("relay","1"); //Publie la demande de chauffage a 1
     }
-    else if(t > c)
+    else if(t > c) //Sinon si la temperature est supérieur a la consigne
     {
-      client.publish("DomoHouse","0");
+      client.publish("relay","0"); //Publie l'arrêt du chauffage.
     }
   }
 }
-
 
 
